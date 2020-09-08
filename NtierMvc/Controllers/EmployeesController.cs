991bff0d252f9b8012cb6ee48e8d117a158760d4
Marimuthu Sakthivel <psmarimuthu@hotmail.com>
2020-09-08 16:44:44 +0530

    Test commit

--- NtierMvc/Controllers/EmployeesController.cs
+++ NtierMvc/Controllers/EmployeesController.cs
@@ -13,7 +13,7 @@ namespace NtierMvc.Controllers
     public class EmployeesController : Controller
     {
         private LoggingHandler _loggingHandler;
-        //Test
+        //Test2
 
         public EmployeesController()
         {
